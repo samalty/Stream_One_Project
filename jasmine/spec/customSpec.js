acabe06d39b697c89f2b97f70541780adbc7374d
Sam Alty <34246374+samalty@users.noreply.github.com>
2020-01-31 09:52:18 +0000

    Update customSpec.js

--- jasmine/spec/customSpec.js
+++ jasmine/spec/customSpec.js
@@ -64,20 +64,20 @@ describe("Form validation", function(){
 
 function validateForm(x, y) {
 
-	//var x = document.forms["contactForm"]["name"].value;
-	//var y = document.forms["contactForm"]["email"].value;
+    //var x = document.forms["contactForm"]["name"].value;
+    //var y = document.forms["contactForm"]["email"].value;
 	
-	if (x == "") {
-    	alert("Please ensure that you have entered your name.");
+    if (x == "") {
+        alert("Please ensure that you have entered your name.");
     	return false;
-	} else if (y == "") {
-		alert("Please ensure that you have entered your email address.");
-		return false;
-	} else if (y.includes("@")) {
-	    alert("Thanks. We'll be in touch with you soon.");
-	    return true;
-	} else {
-	    alert("Please enter a valid email address.");
-	    return false;
-	}
+    } else if (y == "") {
+	alert("Please ensure that you have entered your email address.");
+	return false;
+    } else if (y.includes("@")) {
+	alert("Thanks. We'll be in touch with you soon.");
+	return true;
+    } else {
+	alert("Please enter a valid email address.");
+	return false;
+    }
 }
